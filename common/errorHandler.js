f3dc530b58c0009b3e4c316a1c83f843ff1e15fa
Nazmul-Git <nazmul.hasan16021998@gmail.com>
2024-10-20 00:36:35 +0600

    Implement http error handler.

--- common/errorHandler.js
+++ common/errorHandler.js
@@ -1,22 +1,25 @@
- const createError= require('http-errors');
- 
- function notFoundMiddleware(req, res, next){
+const createError = require('http-errors');
+
+function notFoundMiddleware(req, res, next) {
     next(createError(404, 'Your requested content was not found.'));
- }
+}
 
 //  default error handler
- function errorHandler(err, req, res, next){
-    // res.locals.title="Error Page!";
-    // res.render("error");
+function errorHandler(err, req, res, next) {
+    res.locals.error = process.env.NODE_ENV === 'development' ? err : { message: err.message };
+    res.status(err.status || 500);
+    if (res.locals.html) {
+        // html response
+        res.render('error', {
+            title: 'Error Page!',
+        })
+    } else {
+        // json response
+        res.json(res.locals.error);
+    }
+}
 
-    // or
-    
-    res.render('error', {
-        title: 'Error Page!'
-    });
- }
-
- module.exports = {
+module.exports = {
     notFoundMiddleware,
     errorHandler,
- }
+}
